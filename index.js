efab162795a7ae7018e54d18f16527db8b858d09
Damian <przpiw@gmail.com>
2021-08-01 02:40:23 +0930

    changes

--- index.js
+++ index.js
@@ -32,9 +32,7 @@ const connectWithRetry = () => {
 connectWithRetry()
 app.enable('trust proxy')
 app.get('/api/v1', (req, res) => {
-  res.send(
-    `<h2>Hi 2there from Docker Container in ${process.env.NODE_ENV}</h2>`
-  )
+  res.send(`<h2>Hi there from Docker Container in ${process.env.NODE_ENV}</h2>`)
 })
 
 app.use('/api/v1/users', userRouter)
